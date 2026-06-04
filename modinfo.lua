cfba59e2c4def0d699d6434587d5c5082761ee57
Albi <17704110+pietralbi@users.noreply.github.com>
2026-06-04 14:07:39 -0400

    version bump

--- modinfo.lua
+++ modinfo.lua
@@ -3,7 +3,7 @@ name = "Extra builds"
 description = "Makes extra structures cradftable, including catcoon house, mermhouses, marble tree and mushroom planter"
 author = "Alberto Pietralunga"
 
-version = "1.4.2"
+version = "1.4.3"
 forumthread = ""
 
 api_version = 6
